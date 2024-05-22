c85412371f344e534a2ca3d3dd058917f85d7dde
Kévin Bibollet <bibollet.kevin@gmail.com>
2024-05-22 12:42:54 +0200

    untracked bin and obj directories from git

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TheKartersAssistant
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "TheKartersAssistant";
-        public const string PLUGIN_NAME = "TheKartersAssistant";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
