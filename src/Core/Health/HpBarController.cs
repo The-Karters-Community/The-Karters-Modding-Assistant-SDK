37a69ef81bff30c1e1ad400db423d6c365e4e093
Kévin Bibollet <bibollet.kevin@gmail.com>
2024-05-27 21:21:06 +0200

    PlayerEvent.onItemHit and PlayerEvent.onHeal now only runs when HP or damages are more than 0

--- src/Core/Health/HpBarController.cs
+++ src/Core/Health/HpBarController.cs
@@ -6,31 +6,39 @@ namespace TheKartersModdingAssistant.Core;
 [HarmonyPatch(typeof(HpBarController), nameof(HpBarController.RefillHp))]
 public class HpBarController__RefillHp {
     public static void Prefix(HpBarController __instance, ref int hp, ref bool overdrive) {
-        Player player = Player.FindByAntPlayer(__instance.player);
+        if (hp > 0) {
+            Player player = Player.FindByAntPlayer(__instance.player);
 
-        PlayerEvent.onHeal?.Invoke(player, hp, overdrive);
+            PlayerEvent.onHeal?.Invoke(player, hp, overdrive);
+        }
     }
 
     public static void Postfix(HpBarController __instance, int hp, bool overdrive) {
-        Player player = Player.FindByAntPlayer(__instance.player);
+        if (hp > 0) {
+            Player player = Player.FindByAntPlayer(__instance.player);
 
-        PlayerEvent.onHealAfter?.Invoke(player, hp, overdrive);
+            PlayerEvent.onHealAfter?.Invoke(player, hp, overdrive);
+        }
     }
 }
 
 [HarmonyPatch(typeof(HpBarController), nameof(HpBarController.Hit))]
 public class HpBarController__Hit {
     public static void Prefix(HpBarController __instance, ref int damage, ref int playerMakingDamage, ref PixelWeaponObject.EWeaponType eWeaponType) {
-        Player player = Player.FindByAntPlayer(__instance.player);
-        Player authorPlayer = Player.FindByIndex((Ant_Player.EAntPlayerNumber)playerMakingDamage);
+        if (damage > 0) {
+            Player player = Player.FindByAntPlayer(__instance.player);
+            Player authorPlayer = Player.FindByIndex((Ant_Player.EAntPlayerNumber)playerMakingDamage);
 
-        PlayerEvent.onItemHit?.Invoke(player, damage, authorPlayer, (Item)eWeaponType);
+            PlayerEvent.onItemHit?.Invoke(player, damage, authorPlayer, (Item)eWeaponType);
+        }
     }
 
     public static void Postfix(HpBarController __instance, int damage, int playerMakingDamage, PixelWeaponObject.EWeaponType eWeaponType) {
-        Player player = Player.FindByAntPlayer(__instance.player);
-        Player authorPlayer = Player.FindByIndex((Ant_Player.EAntPlayerNumber)playerMakingDamage);
+        if (damage > 0) {
+            Player player = Player.FindByAntPlayer(__instance.player);
+            Player authorPlayer = Player.FindByIndex((Ant_Player.EAntPlayerNumber)playerMakingDamage);
 
-        PlayerEvent.onItemHitAfter?.Invoke(player, damage, authorPlayer, (Item)eWeaponType);
+            PlayerEvent.onItemHitAfter?.Invoke(player, damage, authorPlayer, (Item)eWeaponType);
+        }
     }
 }
